064207214fd6fad9c0823e36a4b6dd82477fa51e
elifcetinkaya <eliifctnk@gmail.com>
2021-06-01 12:54:48 +0300

    create react app for hooks

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import EmployeeList from './components/EmployeeList';
+
+function App() {
+    return (
+        <div className="App">
+            <EmployeeList></EmployeeList>
+
+        </div>
+    );
+}
+
+export default App;
